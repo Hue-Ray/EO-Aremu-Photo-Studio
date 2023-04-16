7595a60aa1658b9dbb148fc5298a39e4617b420f
Hue-Ray <kunletobi4@gmail.com>
2023-04-16 15:55:39 +0100

    removed the hero bg and added services

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import HeroSection from './components/hero-section/HeroSection'
+import Service from './components/service-slider/Service'
 
 function App() {
   return (
     <div className="App">
-     <HeroSection/>   
+     <HeroSection/>  
+     <Service /> 
     </div>
   )
 }
---
+++ src/components/service-slider/Service.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import serviceImage1 from '../../assets/w-s.png';
+import serviceImage2 from '../../assets/b-s.png';
+import serviceImage3 from '../../assets/o-s.png';
+
+function Service() {
+  return (
+    <div>
+        <section>
+            <div className='flex-col text-center py-4'>
+                <h1 className='heading'>Services</h1>
+
+                <div className='flex justify-center text-center'>
+                    <div>
+                    <p className='text-xs opacity-70 md:text-sm lg:text-md'>Wedding</p>
+                    <img className='lg:hover:w-[182px] transition-all duration-900' src={serviceImage1} alt="wedding service image" />
+                    </div>
+               
+                    <div>
+                    <p className='text-xs opacity-70 md:text-sm lg:text-md'>Birthdays</p>
+                    <img className='lg:hover:w-[182px] transition duration-[3s] ease-out' src={serviceImage2} alt="wedding service image" />
+                    </div>
+
+                    <div>
+                    <p className='text-xs opacity-70 md:text-sm lg:text-md'>Occasions</p>
+                    <img className='lg:hover:w-[182px] transition-all duration-900' src={serviceImage3} alt="wedding service image" />
+                   </div>
+                </div>
+                  <button className='btn text-white'>
+                    Reach Out
+                   </button>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Service
