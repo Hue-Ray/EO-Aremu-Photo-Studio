caa8f7a575d256a306c55d9896651afba4fa3a5a
Hue-Ray <kunletobi4@gmail.com>
2023-04-19 10:52:47 +0100

    worked on the nav bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,50 @@
 import React from 'react'
-import menuIcon from '../../assets/mobile-menu-icon.svg'
 
 function Header() {
+  const closeBtn = document.querySelector('.hide-nav-btn');
+  const showBtn = document.querySelectorAll('.show-nav-btn');
+  const mobileNav = document.querySelector('.nav-menu');
+
+    const showNav = () => {  
+      mobileNav.classList.remove("hide-nav")
+ }
+
+ const hideNav = () => {
+   mobileNav.classList.add("hide-nav")
+ }
+
   return (
     <div>
-     <header className='header-container text-white  flex justify-between 
-     py-2 px-4'>
-     <div>
-        <p className='site-title font-bold text-xl md:text-2xl'>EO-Aremu</p>
-      </div>
+      <header className='text-white py-2 px-4 flex-col justify-center items-center'>
+      <div className='md:hidden'>
+      <div className='flex justify-between'>
+       <p className='logo font-bold'>EO Aremu Studio</p>
+        <div onClick={showNav} className='show-nav-btn transition-all'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+
+        </div>
+       </div>
+        <nav className='nav-menu hide-nav flex-col bg-black
+         p-4 h-[40vh] bg-opacity-70 text-center rounded-lg 
+         mt-4 transition-all duration-[3s]'>
+          
+        <div onClick={hideNav} className='hide-nav-btn'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" 
+        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+        </div>
 
 
-      <div>
-        <ul className='hidden md:flex lg:text-md'>
-          <li className='nav-menu'>About</li>
-          <li className='nav-menu'>Gallery</li>
-          <li className='nav-menu'>Services</li>
-          <li className='nav-menu'>Contact</li>
-        </ul>
-        <img className='mt-2 cursor-pointer md:hidden' src={menuIcon}/>
+          <p className='nav-menu-link p-4'><a href="#about">About</a></p>
+          <p className='nav-menu-link p-4'><a href="#Services">Services</a></p>
+          <p className='nav-menu-link p-4'><a href="#contact">Contact</a></p>
+        </nav>
       </div>
-     </header>
-    </div>
+      </header>
+   </div>
   )
 }
 
